90af579b5759901f842a61269e7167982391ec8b
Elienaldo de Jesus Bittencourt <elienaldo@gmail.com>
2020-04-29 19:37:41 -0300

    Alterações na Main Page.

--- AppProCiencia/AppProCiencia/App.xaml.cs
+++ AppProCiencia/AppProCiencia/App.xaml.cs
@@ -10,7 +10,8 @@ namespace AppProCiencia
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new MainPage();
+            //MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
